693b5f577f05b06076e3e3dedf4599c57a14e4f3
andersasplundberggren <anders.asplundberggren@gmail.com>
2025-10-19 21:24:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(cors({
         'http://localhost:3000',
         'http://127.0.0.1:5500'
     ],
-    credentials: true
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
